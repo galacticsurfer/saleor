df3f68ddfad217fad28a00b08b8ac530189ae899
Sachin Prabhu <galacticsurfer@gmail.com>
2016-03-15 00:16:53 +0530

    Added voucher to topspin

--- saleor/core/views.py
+++ saleor/core/views.py
@@ -1,12 +1,16 @@
 from django.template.response import TemplateResponse
 
 from ..product.models import Product
+from .utils import get_paginator_items
+from django.conf import settings
 
 
 def home(request):
-    products = Product.objects.get_available_products()[:12]
+    products = Product.objects.get_available_products()
     products = products.prefetch_related('categories', 'images',
                                          'variants__stock')
+    products = get_paginator_items(
+        products, settings.PAGINATE_BY, request.GET.get('page'))
     return TemplateResponse(
         request, 'base.html',
         {'products': products, 'parent': None})
