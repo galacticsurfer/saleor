ea96f32e5b96cc8941c549f85b8e7d362bdfcbb2
Sachin Prabhu <sachin.prabhu@kuliza.com>
2017-03-01 02:03:19 +0530

    Changed state selection to dropdown

--- saleor/userprofile/models.py
+++ saleor/userprofile/models.py
@@ -29,6 +29,20 @@ class AddressManager(models.Manager):
         address, dummy_created = user.addresses.get_or_create(**data)
         return address
 
+STATE_CHOICES = (('Andaman and Nicobar Islands', 'Andaman and Nicobar Islands'), ('Andhra Pradesh', 'Andhra Pradesh'),
+                 ('Arunachal Pradesh', 'Arunachal Pradesh'), ('Assam', 'Assam'), ('Bihar', 'Bihar'),
+                 ('Chandigarh', 'Chandigarh'), ('Chhattisgarh', 'Chhattisgarh'),
+                 ('Dadra and Nagar Haveli', 'Dadra and Nagar Haveli'), ('Daman and Diu', 'Daman and Diu'),
+                 ('Delhi', 'Delhi'), ('Goa', 'Goa'), ('Gujarat', 'Gujarat'), ('Haryana', 'Haryana'),
+                 ('Himachal Pradesh', 'Himachal Pradesh'), ('Jammu and Kashmir', 'Jammu and Kashmir'),
+                 ('Jharkhand', 'Jharkhand'), ('Karnataka', 'Karnataka'), ('Kerala', 'Kerala'),
+                 ('Lakshadweep', 'Lakshadweep'), ('Madhya Pradesh', 'Madhya Pradesh'), ('Maharashtra', 'Maharashtra'),
+                 ('Mumbai', 'Mumbai'), ('Manipur', 'Manipur'), ('Meghalaya', 'Meghalaya'), ('Mizoram', 'Mizoram'),
+                 ('Nagaland', 'Nagaland'), ('Odisha', 'Odisha'), ('Puducherry', 'Puducherry'), ('Punjab', 'Punjab'),
+                 ('Rajasthan', 'Rajasthan'), ('Sikkim', 'Sikkim'), ('Tamil Nadu', 'Tamil Nadu'),
+                 ('Telangana', 'Telangana'), ('Tripura', 'Tripura'), ('Uttar Pradesh', 'Uttar Pradesh'),
+                 ('Uttarakhand', 'Uttarakhand'), ('West Bengal', 'West Bengal'))
+
 
 @python_2_unicode_compatible
 class Address(models.Model):
@@ -60,7 +74,7 @@ class Address(models.Model):
         pgettext_lazy('Address field', 'country'))
     country_area = models.CharField(
         pgettext_lazy('Address field', 'state or province'),
-        max_length=128, blank=True)
+        max_length=128, blank=True, choices=STATE_CHOICES)
     phone = models.CharField(
         pgettext_lazy('Address field', 'phone number'),
         max_length=30, blank=True)
