2f22f9e9d9599a7c90f317dd48f3d3c5338a4608
Sachin Prabhu <sachin.prabhu@kuliza.com>
2017-03-18 00:58:24 +0530

    Added one dream promo

--- saleor/core/urls.py
+++ saleor/core/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.views.generic import RedirectView
 
 from . import views
 
 
 urlpatterns = [
+    url(r'^onedream/$', RedirectView.as_view(url='https://goo.gl/forms/ClAVIeK1eDfhqGq92')),
     url(r'^$', views.home, name='home')
+
 ]
